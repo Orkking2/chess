be989ee4c6f0879953e9c4440d95d92b0ee12816
Nicolas Bird von Eyben <nicolas.bird.voneyben@gmail.com>
2025-03-07 15:36:42 -0800

    inline fn from: Color -> bool

--- src/color.rs
+++ src/color.rs
@@ -86,6 +86,7 @@ impl Not for Color {
 impl From<Color> for bool {
     /// While in the backend, `Color::White == 0` and `Color::Black == 1`, 
     /// it is more intuitive for `Color::White` to evaluate `true`, as it goes first
+    #[inline]
     fn from(value: Color) -> Self {
         match value {
             Color::White => true,
