6cbd82308ae8fc8e651af32d23252751d2d50a1e
Nicolas Bird von Eyben <nicolas.bird.voneyben@gmail.com>
2025-03-10 19:16:10 -0700

    Remove Fail dependency (unmaintained crate)

--- src/error.rs
+++ src/error.rs
@@ -1,42 +1,47 @@
-use failure::Fail;
+use std::fmt;
 
 /// Sometimes, bad stuff happens.
-#[derive(Clone, Debug, Fail)]
-#[cfg(feature="std")]
+#[derive(Clone, Debug)]
+#[cfg(feature = "std")]
 pub enum Error {
     /// The FEN string is invalid
-    #[fail(display = "Invalid FEN string: {}", fen)]
     InvalidFen { fen: String },
 
     /// The board created from BoardBuilder was found to be invalid
-    #[fail(
-        display = "The board specified did not pass sanity checks.  Are you sure the kings exist and the side to move cannot capture the opposing king?"
-    )]
     InvalidBoard,
 
     /// An attempt was made to create a square from an invalid string
-    #[fail(display = "The string specified does not contain a valid algebraic notation square.")]
     InvalidSquare,
 
     /// An attempt was made to create a move from an invalid SAN string
-    #[fail(display = "The string specified does not contain a valid SAN notation move")]
     InvalidSanMove,
 
     /// An atempt was made to create a move from an invalid UCI string
-    #[fail(display = "The string specified does not contain a valid UCI notation move")]
     InvalidUciMove,
 
     /// An attempt was made to convert a string not equal to "1"-"8" to a rank
-    #[fail(display = "The string specified does not contain a valid rank.")]
     InvalidRank,
 
     /// An attempt was made to convert a string not equal to "a"-"h" to a file
-    #[fail(display = "The string specified does not contain a valid file.")]
     InvalidFile,
 }
 
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            Self::InvalidFen{ fen: s } => write!(f, "Invalid FEN string: {}", s),
+            Self::InvalidBoard => write!(f, "The board specified did not pass sanity checks.  Are you sure the kings exist and the side to move cannot capture the opposing king?"),
+            Self::InvalidSquare => write!(f, "The string specified does not contain a valid algebraic notation square."),
+            Self::InvalidSanMove => write!(f, "The string specified does not contain a valid SAN notation move"),
+            Self::InvalidUciMove => write!(f, "The string specified does not contain a valid UCI notation move"),
+            Self::InvalidRank => write!(f, "The string specified does not contain a valid rank."),
+            Self::InvalidFile => write!(f, "The string specified does not contain a valid file.")
+        }
+    }
+}
+
 #[derive(Clone, Debug)]
-#[cfg(not(feature="std"))]
+#[cfg(not(feature = "std"))]
 pub enum Error {
     InvalidFen,
     InvalidBoard,
